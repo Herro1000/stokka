319395cfa64daff1bb8b804af52278eeb5a4427b
Jabekah007 <70330339+Jabekah007@users.noreply.github.com>
2022-07-16 18:55:13 +0000

    i created database

---
+++ ecgproj/stokka/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from datetime import  datetime
+# Create your models here.
+'''
+ ASSET_CHOICES = (
+         ('Laptop'),
+        ('Desktop'),
+        ('UPS'),
+        ('Printer'),
+        ('Scanner'),
+)
+
+'''                               
+
+
+       
+
+class Product(models.Model):
+
+    Asset_Type = models.CharField( max_length=15)
+    Asset_Name = models.CharField(max_length=15)
+    Serial_No =  models.CharField(max_length=15)
+    Mac_address = models.CharField(max_length=15)
+    Memmory_Size = models.IntegerField()
+    HHD_Size = models.IntegerField()
+    Asset_Model= models.CharField(max_length=15)
+    time_created = models.DateTimeField(default=datetime.now)
+
+    def __str__(self) :
+        return self.Asset_Name                    
+
+
+
+
+
+    
+
+    
+
+
+
+
